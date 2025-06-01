d71b0b79caadd765886da5e02073779af6683b3a
Vitor Kato <vitor48kato@gmail.com>
2025-06-01 19:18:00 -0300

    Projeto criado

---
+++ src/test/java/vitor/kato/uolhost_backend/UolhostBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vitor.kato.uolhost_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UolhostBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
